511663a8711afbc7dca3378f5d9452317e9635c2
jonathansantss <142455956+jonathansantss@users.noreply.github.com>
2023-08-22 21:47:17 -0300

    Create 2exercicios.js

---
+++ 2exercicios.js
@@ -0,0 +1,38 @@
+//let variavelTexto = "texto"; 
+//let variavelNumero = 6;
+//console.log(typeof variavelTexto);
+//console.log(typeof variavelNumero);
+
+//variavel vazia
+//let variavelVazia;
+//console.log(typeof variavelVazia); 
+//let variavelnula = null;
+//console.log(variavelnula);
+
+let a = "b";
+console.log(a);
+console.log(typeof a);
+
+a = null;
+console.log(a);
+console.log(typeof a)
+
+//exercicio 2
+
+let nome;
+let idade;
+let numero = 6;
+nome = prompt('QUAL É O SEU NOME?');
+idade = prompt('QUAL A SUA IDADE?');
+console.log(typeof nome);
+console.log(typeof idade);
+console.log(numero + 5);
+console.log(numero.toString() + 5);
+
+
+//conversao numero 
+let n = '42';
+console.log (number(n));
+console.log(typeof n);
+console.log(typeof number (n));
+
